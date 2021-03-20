ed5f53f372c5eb7f5829e490c01d9b7297ec75d0
RozaG <72781650+RozaGu@users.noreply.github.com>
2021-03-20 01:01:48 -0400

    SidebarOption

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,86 +1,87 @@
-import React from "react"
-import styled from "styled-components"
-import { Avatar } from "@material-ui/core"
+import React from "react";
+import styled from "styled-components";
+import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import SearchIcon from "@material-ui/icons/Search";
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 const Header = () => {
-    return (
-			<HeaderContainer>
-				{/* <h1>slack</h1> */}
-				{/* Header Left  */}
-				<HeaderLeft>
-					<HeaderAvatar />
-					<AccessTimeIcon />
-				</HeaderLeft>
-				<HeaderSearch>
-                <SearchIcon />
-                <input placeholder="Search"/>
-            </HeaderSearch>
+	return (
+		<HeaderContainer>
+			{/* <h1>slack</h1> */}
+			{/* Header Left  */}
+			<HeaderLeft>
+				<HeaderAvatar />
+				<AccessTimeIcon />
+			</HeaderLeft>
+			<HeaderSearch>
+				<SearchIcon />
+				<input placeholder="Search" />
+			</HeaderSearch>
 
-            <HeaderRight>
-                <HelpOutlineIcon/>
-            </HeaderRight>
-			</HeaderContainer>
-		);
-}
+			<HeaderRight>
+				<HelpOutlineIcon />
+			</HeaderRight>
+		</HeaderContainer>
+	);
+};
 export default Header;
 const HeaderRight = styled.div`
-cursor:pointer;
-flex:0.3;
-display:flex;
-align-items:flex-end;
-> .MuiSvgIcon-root{
-    margin-left:auto ;
-    margin-right:20px;
-
-}
+	cursor: pointer;
+	flex: 0.3;
+	display: flex;
+	align-items: flex-end;
+	> .MuiSvgIcon-root {
+		margin-left: auto;
+		margin-right: 20px;
+	}
 `;
-
 
 const HeaderSearch = styled.div`
-flex:0.4;
-opacity:1;
-text-align:center;
-background-color:#421f44;
-border-radius:6px;
-display:flex;
-padding:0 50px;
-color:gray;
-border:1px gray solid;
-> input{
-    border:none;
-    text-align:center;
-    background-color:transparent;
-    min-width:30vw;
-    outline:none;
-    color:white;
-}
+	flex: 0.4;
+	opacity: 1;
+	text-align: center;
+	background-color: #421f44;
+	border-radius: 6px;
+	display: flex;
+	padding: 0 50px;
+	color: gray;
+	border: 1px gray solid;
+	> input {
+		border: none;
+		text-align: center;
+		background-color: transparent;
+		min-width: 30vw;
+		outline: none;
+		color: white;
+	}
 `;
 
-const HeaderContainer = styled.div`color:red;
-display:flex;
-position:fixed;
-width:100%;
-align-items:center;
-justify-content:space-between;
-padding:10px 0;
-background-color:var(--purple);
-color: white;
+const HeaderContainer = styled.div`
+	color: red;
+	display: flex;
+	position: fixed;
+	width: 100%;
+	align-items: center;
+	justify-content: space-between;
+	padding: 10px 0;
+	background-color: var(--purple);
+	color: white;
 `;
 const HeaderLeft = styled.div`
-flex:0.3;
-display:flex;
-align-items:center;
-margin-left:20px;
->.MuiSvgIcon-root{
-    margin-right:30px;
-    margin-left:auto;
-}`;
+	flex: 0.3;
+	display: flex;
+	align-items: center;
+	margin-left: 20px;
+	> .MuiSvgIcon-root {
+		margin-right: 30px;
+		margin-left: auto;
+	}
+`;
 
 const HeaderAvatar = styled(Avatar)`
-cursor:pointer;
-:hover{
-    color:var(--purple);
-    opacity:0.8;
-}`;
+	cursor: pointer;
+	:hover {
+		color: var(--purple);
+		opacity: 0.8;
+	}
+`;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,17 @@ import styled from "styled-components";
 import React from "react";
 import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 import CreateIcon from "@material-ui/icons/Create";
+import SidebarOption from "./SidebarOption";
+import InsertCommentIcon from "@material-ui/icons/InsertComment";
+import InboxIcon from "@material-ui/icons/Inbox";
+import DraftsIcons from "@material-ui/icons/Drafts";
+import BookmarkBorderIcon from "@material-ui/icons/BookmarkBorder";
+import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
+import AppsIcon from "@material-ui/icons/Apps";
+import FileCopyIcon from "@material-ui/icons/FileCopy";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
 
 function Sidebar() {
 	return (
@@ -16,6 +27,18 @@ function Sidebar() {
 				</SidebarInfo>
 				<CreateIcon />
 			</SidebarHeader>
+			<SidebarOption Icon={InsertCommentIcon} title="Threads" />
+			<SidebarOption Icon={InboxIcon} title="Mentions and reactions" />
+			<SidebarOption Icon={DraftsIcons} title="Saved items" />
+			<SidebarOption Icon={BookmarkBorderIcon} title="Channel browser" />
+			<SidebarOption Icon={PeopleAltIcon} title="People & user groups" />
+			<SidebarOption Icon={AppsIcon} title="Apps" />
+			<SidebarOption Icon={FileCopyIcon} title="File browser" />
+			<SidebarOption Icon={ExpandLessIcon} title="Show less" />
+			<hr />
+            <SidebarOption Icon={ExpandMoreIcon} title="Channell" />
+            <hr/>
+			<SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
 		</SidebarContainer>
 	);
 }
@@ -58,4 +81,10 @@ const SidebarInfo = styled.div`
 		font-weight: 400;
 		align-items: center;
 	}
+    >h3 .MuiSvgIcon-root{
+        font-size:14px;
+        margin-top:1px;
+        margin-right:2px;
+        color:green;
+    }
 `;
